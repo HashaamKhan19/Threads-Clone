ffef7350b6839206b27b1308e7cee0831bb83126
hashaam <hashaamkhan4247@gmail.com>
2023-11-28 16:16:00 +0500

    feat:delete thread

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,15 +1,21 @@
-import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
-  const userInfo = {};
+  if (!user) return null; // to avoid typescript warnings
+
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
 
   const userData = {
-    id: user?.id,
+    id: user.id,
     objectId: userInfo?._id,
-    username: userInfo ? userInfo?.username : user?.username,
-    name: userInfo ? userInfo?.name : user?.firstName ?? "",
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
     bio: userInfo ? userInfo?.bio : "",
     image: userInfo ? userInfo?.image : user.imageUrl,
   };
@@ -27,4 +33,5 @@ async function Page() {
     </main>
   );
 }
+
 export default Page;
